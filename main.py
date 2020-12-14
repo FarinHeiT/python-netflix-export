17d61974ced4549c112c4afde930436fe58a6ab7
FarinHeiT <yariksuharik85@gmail.com>
2020-12-14 15:22:02 +0200

    Refactoring, driver factory, logged in driver clones creation

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 NETFLIX_URL = "https://www.netflix.com/"
 
-# Disable selenium logging and make browser headless
-chrome_options = webdriver.ChromeOptions()
-chrome_options.headless = True
-chrome_options.add_argument("--log-level=3")
-chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(options=chrome_options)
+
+def get_driver():
+    # Disable selenium logging and make browser headless
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.headless = True
+    chrome_options.add_argument("--log-level=3")
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome()
+
+    return driver
 
 
-@click.command()
-@click.option('--email', help='Email to your Netflix account.', prompt='Your Email')
-@click.option('--password', help='Password to your Netflix account.', prompt='Your Password', hide_input=True)
 def login(email, password):
+    """ Tries to log in and return the Logged In driver """
     click.echo("Trying to log in...")
+
+    driver = get_driver()
+
     driver.get(NETFLIX_URL + 'login')
     login_input = driver.find_element_by_name("userLoginId")
     password_input = driver.find_element_by_name("password")
@@ -37,10 +43,37 @@ def login(email, password):
 
         print("Successfully logged in.")
 
+        return driver
+
     except TimeoutException as e:
         print("Invalid login/passowrd. Please check your credentials and try again.")
         driver.quit()
 
 
+def get_logged_in_driver_clone(driver):
+    """ Returns a Logged In clone of driver using cookies"""
+    driver_clone = get_driver()
+    driver_clone.get(NETFLIX_URL)
+    cookies = driver.get_cookies()
+
+    for cookie in cookies:
+        driver_clone.add_cookie(cookie)
+
+    return driver_clone
+
+
+@click.command()
+@click.option('--email', help='Email to your Netflix account.', prompt='Your Email')
+@click.option('--password', help='Password to your Netflix account.', prompt='Your Password', hide_input=True)
+@click.option('--action', help='Password to your Netflix account.', type=click.Choice(['import', 'export']))
+def main(email, password, action):
+    logged_in_driver = login(email, password)
+
+    if action == 'export':
+        clone = get_logged_in_driver_clone(logged_in_driver)
+        clone.get(NETFLIX_URL + "YourAccount")
+        clone.refresh()
+
+
 if __name__ == "__main__":
-    login()
+    main()
