8adf6d019074c990745589268917e309296fb6d7
FarinHeiT <yariksuharik85@gmail.com>
2020-12-15 16:43:35 +0200

    get_profiles, profiles handling via CLI

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ NETFLIX_URL = "https://www.netflix.com/"
 
 
 def get_driver():
+    """ Get an instance of headless driver """
+
     # Disable selenium logging and make browser headless
     chrome_options = webdriver.ChromeOptions()
     chrome_options.headless = True
@@ -20,6 +22,7 @@ def get_driver():
 
 def login(email, password):
     """ Tries to log in and return the Logged In driver """
+
     click.echo("Trying to log in...")
 
     driver = get_driver()
@@ -46,12 +49,13 @@ def login(email, password):
         return driver
 
     except TimeoutException as e:
-        print("Invalid login/passowrd. Please check your credentials and try again.")
+        print("Invalid login/password. Please check your credentials and try again.")
         driver.quit()
 
 
 def get_logged_in_driver_clone(driver):
-    """ Returns a Logged In clone of driver using cookies"""
+    """ Returns a Logged In clone of driver using cookies """
+
     driver_clone = get_driver()
     driver_clone.get(NETFLIX_URL)
     cookies = driver.get_cookies()
@@ -59,20 +63,44 @@ def get_logged_in_driver_clone(driver):
     for cookie in cookies:
         driver_clone.add_cookie(cookie)
 
+    driver_clone.refresh()
+
     return driver_clone
+
+
+def get_profiles(logged_in_driver):
+    profile_divs = logged_in_driver.find_elements_by_class_name("profile-link")
+    profiles = {}
+    for profile_div in profile_divs:
+        name = profile_div.find_element_by_class_name("profile-name").get_attribute("innerHTML")
+        uid = profile_div.find_element_by_class_name("profile-icon").get_attribute("data-profile-guid")
+        profiles[name] = uid
+
+    return profiles
 
 
 @click.command()
 @click.option('--email', help='Email to your Netflix account.', prompt='Your Email')
 @click.option('--password', help='Password to your Netflix account.', prompt='Your Password', hide_input=True)
-@click.option('--action', help='Password to your Netflix account.', type=click.Choice(['import', 'export']))
-def main(email, password, action):
+@click.option('-p',
+              help='Profile that you want to process '
+                   '(you can specify more accounts by '
+                   'reusing this flag).',
+              multiple=True)
+@click.option('--action',
+              help='What action do you want to perform: '
+                   'import (upload data to Netflix) or '
+                   'export download data from Netflix)',
+              type=click.Choice(['import', 'export']))
+def main(email, password, p, action):
     logged_in_driver = login(email, password)
+    profiles = get_profiles(logged_in_driver)
+    print(f"Found {len(profiles.keys)} profiles: {', '.join(profiles.keys)}.")
+
 
     if action == 'export':
+        print(f"Exporting following profiles: {', '.join(p)}")
         clone = get_logged_in_driver_clone(logged_in_driver)
-        clone.get(NETFLIX_URL + "YourAccount")
-        clone.refresh()
 
 
 if __name__ == "__main__":
